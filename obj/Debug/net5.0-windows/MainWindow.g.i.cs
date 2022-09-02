52853d7375c0e9be1a5a4fe6127cccfaf7beffc9
Mark Hauenstein <markhauen@gmail.com>
2022-09-02 10:25:26 -0500

    renamed project

--- obj/Debug/net5.0-windows/MainWindow.g.i.cs
+++ obj/Debug/net5.0-windows/MainWindow.g.i.cs
@@ -197,7 +197,7 @@ namespace ExcelReader {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/ExcelReader;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/DataCompanion;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
